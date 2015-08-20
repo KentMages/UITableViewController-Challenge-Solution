8a40e1e99a222c20375a4555e03bb132be079406
Kent H. Mages <kent.mages@gmail.com>
2015-08-20 16:52:27 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Kent H. Mages on 8/20/15.
+//  Copyright © 2015 Kent H. Mages. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
